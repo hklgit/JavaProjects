5c8326c8902c2e186fd2dfe7e9cadb18a5bb2853
hankunlun <811653654@qq.com>
2017-12-04 23:08:35 +0800

    用于java学习的工程

---
+++ src/main/java/com/coocaa/conf/ConfigurationManager.java
@@ -0,0 +1,16 @@
+package com.coocaa.conf;
+
+import java.util.Properties;
+
+public class ConfigurationManager {
+    private  static Properties prop=new Properties();
+    /*
+    静态代码块,类第一次使用的时候就会被初始化
+    * */
+    static{
+
+
+    }
+
+
+}
