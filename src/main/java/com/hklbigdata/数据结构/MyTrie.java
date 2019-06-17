4ec49fd5e37cffab9e9a561902a08f124a8a9c6b
hankunlun <811653654@qq.com>
2019-06-17 19:04:29 +0800

    synchronized....

---
+++ src/main/java/com/hklbigdata/数据结构/MyTrie.java
@@ -0,0 +1,33 @@
+package com.hklbigdata.数据结构;
+
+/**
+ * Created by Kerven on 2019/6/17.
+ * 参考链接：https://blog.csdn.net/abcd_d_/article/details/40116485
+ * <p>
+ * 　　　　　　　   ┏┓　   ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ +
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * <p>
+ * Don't bibi , show me the code
+ * 字典树走起
+ */
+public class MyTrie {
+}
