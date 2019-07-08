43dceaaf5e9cda1a2ac6eaa31cc3eea19e471440
hankunlun <811653654@qq.com>
2019-07-08 14:14:08 +0800

    修改了一个bug....

--- src/main/java/com/hklbigdata/数据结构/Heap.java
+++ src/main/java/com/hklbigdata/数据结构/Heap.java
@@ -9,24 +9,38 @@ public class Heap {
 
     public static void main(String[] args) {
 
+        int[] a  = {2,4,34,12,67,88,22,56};
+
+        //构建&排序
+        minHeap_Sort(a,a.length);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+
+
+
     }
 
-    public static void MinHeap_Sort(int a[], int n) {
+    public static void minHeap_Sort(int a[], int n) {
             int temp = 0;
-             MakeMinHeap(a, n);
+             makeMinHeap(a, n);
+             //我们是从0开始编号，所以是
             for (int i = n - 1; i > 0; i--) {
                      temp = a[0];
                      a[0] = a[i];
                      a[i] = temp;
-                    MinHeapFixdown(a, 0, i);
+                    minHeapFixdown(a, 0, i);
             }
           }
 
 
     //构建最小堆
-    public static void MakeMinHeap(int a[], int n) {
+    public static void makeMinHeap(int a[], int n) {
+        // i 从第一个非叶子节点开始调整
         for (int i = (n - 1) / 2; i >= 0; i--) {
-            MinHeapFixdown(a, i, n);
+            minHeapFixdown(a, i, n);
         }
     }
 
@@ -35,7 +49,7 @@ public class Heap {
      * @param i      开始调整的位置
      * @param length 数组的长度
      */
-    public static void MinHeapFixdown(int[] arr, int cur, int length) {
+    public static void minHeapFixdown(int[] arr, int cur, int length) {
         //左孩子结点，我们首先从第一个非叶子结点开始找，
         //所以说，他肯定是得有左孩子的，但是是否有右孩子是不确定的
 
@@ -49,7 +63,7 @@ public class Heap {
                 leftChild++;  // 这里就是对进行了重新的赋值，做了一个+1的操作，其实就是从左孩子变为了右孩子
             }
 
-            if (arr[cur] <= arr[leftChild]) {
+            if (arr[cur] >= arr[leftChild]) {
                 break;
             }
 
